fb6685c1dfcd73650a289f461e2b1462ac19698f
Lautaro Zabaleta <92881251+L4U74R022@users.noreply.github.com>
2022-11-19 01:31:11 -0300

    Arreglado el git commit

---
+++ BatallaNaval/Persistence/Conexion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SQLite;
+using System.Windows.Forms;
+
+namespace BatallaNaval.Persistence
+{
+    class Conexion
+    {
+        static String cadena = @"Data Source = BatallaNavalDB.db";
+       public  static SQLiteConnection Connection = new SQLiteConnection(Conexion.cadena);
+        public static void OpenConexion()
+        {
+            if (Connection.State == System.Data.ConnectionState.Closed)
+                Connection.Open();
+        }
+    
+        public static void CloseConexion()
+        {
+            Connection.Close();
+        }
+
+
+        public static SQLiteConnection Connections
+        {
+            set { Connection = value; }
+            get { return Connection; }
+        }
+
+
+    }
+}
---
+++ BatallaNaval/Persistence/pUser.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace BatallaNaval.Persistence
+{
+    class pUser
+    {
+        public static bool authLogin(String User, String Password)
+        {
+            bool auth = false;
+            String password = EncoderMD5.Encode (Password);
+            String user = User;
+            string dbUser = "";
+            string dbPassword = "";
+
+            Conexion.OpenConexion();
+            //query para verificar usuario y contraseña
+            String query = @"SELECT username, password FROM  user WHERE username = @username AND password = @password";
+            SQLiteCommand cmd = new SQLiteCommand(query, Conexion.Connection);
+            cmd.Parameters.Add(new SQLiteParameter ("@username", user));
+            cmd.Parameters.Add(new SQLiteParameter ("@password", password));
+            SQLiteDataReader dr = cmd.ExecuteReader();                       //tobi estuvo aqui
+            while (dr.Read())
+            {
+                dbUser = dr.GetString(0);
+                dbPassword = dr.GetString(1);
+
+            }
+            if (dbUser == user && dbPassword == password)
+            {
+                auth = true;
+            }
+
+            return auth;
+        }
+        public static bool regUser(String name, String lastName, String username, String password, String Email)
+        {
+            bool result = true;
+            string Password = EncoderMD5.Encode(password);
+            //query para  registrar nuevo usuario
+            Conexion.OpenConexion();
+            string query = "INSERT INTO user (name, lastname, username, email, password) VALUES (@name, @lastname,@username,@email,@password)";
+            SQLiteCommand cmd = new SQLiteCommand(query, Conexion.Connection);
+            cmd.Parameters.Add(new SQLiteParameter("@name", name));
+            cmd.Parameters.Add(new SQLiteParameter("@lastname", lastName));
+            cmd.Parameters.Add(new SQLiteParameter("@username", username));
+            cmd.Parameters.Add(new SQLiteParameter("@password", Password));
+            cmd.Parameters.Add(new SQLiteParameter("@email", Email));
+            if (cmd.ExecuteNonQuery()<1)
+            {
+                result = false;
+            }
+            return result;
+        }
+        public static bool verifyUsername(String username)
+        {
+            bool exist = false;
+
+            //query para verificar existencia 
+            return exist;
+        }
+    }
+}
